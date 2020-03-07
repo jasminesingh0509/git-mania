3c1e3b3de44242ff9e6727725e15abff245647b6
joel-wowz <josephlafluer@hotmail.com>
2020-03-07 20:20:57 +0000

    testing branchs

--- array-search.js
+++ array-search.js
@@ -11,30 +11,34 @@ function search(array, item) {
   return index;
 }
 
-/* function search(array, item) {
+function search(array, item) {
   let min = 0; // 1
   let max = array.length - 1; // 1
 
-  while (true) { // lg n
+  while (true) {
+    // lg
     const endIndex = min + max; // lg n
     const middleDecimal = endIndex / 2.0; // lg n
     const middleIndex = Math.floor(middleDecimal); // lg n
 
     const currentItem = array[middleIndex]; // lg n
 
-    if (currentItem === item) { // lg n
+    if (currentItem === item) {
+      // lg n
       return middleIndex;
-    } else if (currentItem < item) { // lg n
+    } else if (currentItem < item) {
+      // lg n
       min = middleIndex + 1;
     } else {
       max = middleIndex - 1; // lg n
     }
 
-    if (min > max) { // lg n
+    if (min > max) {
+      // lg n
       return null; // 1
     }
   }
-} */
+}
 
 /* Compare the largest and smallest numbers
 If the result is the one you looking for, stop
